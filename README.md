e4f5896b57366fea16a8d5149733f4af6ef9a4ba
Hernan Razo <hernanrazo@gmail.com>
2018-07-05 12:32:58 -0500

    Added and reconfigured graphs.

---
+++ README.md
@@ -0,0 +1,16 @@
+Loan Prediction Demo
+===
+
+Description
+---
+A python program that predicts whether or not a loan applicant will get approved. Data includes gender, marital status, income, education level, and many more criteria. This program takes this information and makes a prediction as to whether or the applicant is eligible for a loan. This problem is part of the Loan Prediction Practive Problem at the Analytics Vidhya website. 
+
+Analysis
+---
+First, we can make a few graphs to visualize the data. The 
+
+
+Sources and Helpful links
+---
+https://datahack.analyticsvidhya.com/contest/practice-problem-loan-prediction-iii/  
+https://www.analyticsvidhya.com/blog/2016/01/complete-tutorial-learn-data-science-python-scratch-2/  
--- loan_prediction.py
+++ loan_prediction.py
@@ -6,27 +6,55 @@ import matplotlib.pyplot as plt
 #get training data into a dataframe
 df = pd.read_csv('/Users/hernanrazo/pythonProjects/loan_prediction/train_data.csv')
 
+#make a few graphs to visualize the data and
+#get ideas for later manipulation 
+
 #make a histogram of applicant income
-#save a .png file version of the graph
-histogram = plt.figure()
-plt.title('Histogram of Applicant Income')
+appIncomeHist = plt.figure()
+plt.title('Applicant Income Histogram')
 df['ApplicantIncome'].hist(bins = 50)
-histogram.savefig('hist.png')
+appIncomeHist.savefig('appIncomeHist.png')
 
 #make a box plot of applicant income
-#save a .png file version of the graph
-boxPlot = plt.figure()
-plt.title('Box Plot for Applicant Income')
+appIncomeBoxPlot = plt.figure()
+plt.title('Applicant Income Box Plot')
 df.boxplot(column = 'ApplicantIncome')
-boxPlot.savefig('boxPlot.png')
+appIncomeBoxPlot.savefig('appIncomeBoxPlot.png')
 
 #make another box plot of applicant income but this time,
 #seperate by education levels
-#save a .png file version of the graph
-boxPlotEd = plt.figure()
+educationBoxPlot = plt.figure()
 plt.title('Applicant Income by Education')
 df.boxplot(column = 'ApplicantIncome', by = 'Education')
-boxPlotEd.savefig('boxPlotEd.png')
+educationBoxPlot.savefig('educationBoxPlot.png')
+
+
+#make a histogram of loan amounts
+loanAmountHist = plt.figure()
+plt.title('Loan Amount Value Histogram')
+df['LoanAmount'].hist(bins = 50)
+loanAmountHist.savefig('loanAmountHist.png')
+
+#make a histogram of loan amounts
+loanAmountBoxPlot = plt.figure()
+plt.title('Loan Amount Value Box Plot')
+df.boxplot(column = 'LoanAmount')
+loanAmountBoxPlot.savefig('loanAmountBoxPlot.png')
+
+#start categorical variable analysis
+#make plot that shows likelihood of approval based on various catagories of 
+#the applicants
+
+#graph based on credit history
+ch_ls_graph = plt.figure()
+ch_ls_graph = pd.crosstab(df['Credit_History'], df['Loan_Status'])
+ch_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'], 
+	grid = False, title = 'Likelihood of Approval Based on Credit History')
+plt.savefig('ch_ls_graph.png')
+
+
+
+
 
 
 
