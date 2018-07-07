6991d839807aba89192efc92304a1b0de83f4c78
Hernan Razo <hernanrazo@gmail.com>
2018-07-06 23:57:04 -0500

    made more graphs

--- loan_prediction.py
+++ loan_prediction.py
@@ -42,8 +42,8 @@ df.boxplot(column = 'LoanAmount')
 loanAmountBoxPlot.savefig('loanAmountBoxPlot.png')
 
 #start categorical variable analysis
-#make plot that shows likelihood of approval based on various catagories of 
-#the applicants
+#make plot that shows likelihood of approval based on other catagories of 
+#the applicants' information
 
 #graph based on credit history
 ch_ls_graph = plt.figure()
@@ -52,22 +52,48 @@ ch_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'],
 	grid = False, title = 'Likelihood of Approval Based on Credit History')
 plt.savefig('ch_ls_graph.png')
 
+#graph based on education
+e_ls_graph = plt.figure()
+e_ls_graph = pd.crosstab(df['Credit_History'], df['Loan_Status'])
+e_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'],
+	grid = False, title = 'Likelihood of Approval Based on Education')
+plt.savefig('e_ls_graph.png')
 
 
+#graph based on marital status
+m_ls_graph = plt.figure()
+m_ls_graph = pd.crosstab(df['Credit_History'], df['Married'])
+m_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'],
+	grid = False, title = 'Likelihood of Approval Based on Marital Status')
+plt.savefig('m_ls_graph.png')
 
+#now deal with empty cells and null values in the dataset
+#check for empty cells and null data 
+print(df.apply(lambda x: sum(x.isnull()), axis = 0))
 
+#print empty line for when reading in terminal
+print(' ')
 
+#use a frequency table to see how many self employed applicants
+#got approved or denied for a loan
+print(df['Self_Employed'].value_counts())
 
+#since most self employed applicants get rejected, it is safe to 
+#fill empty cells as 'no'
+df['Self_Employed'].fillna('No', inplace = True)
 
+#create pivot table for values in self employed and education categories
+pivotTable = df.pivot_table(values = 'LoanAmount', index = 'Self_Employed',
+	columns = 'Education', aggfunc = np.median)
 
+def pivot_table(x):
+	return pivotTable.loc[x['Self_Employed'], x['Education']]
 
+#fill missing values
+df['LoanAmount'].fillna(df[df['LoanAmount'].isnull()].apply(pivot_table,
+	axis = 1), inplace = True)
 
-
-
-
-
-
-
+#now analyze outliers and extreme values
 
 
 
