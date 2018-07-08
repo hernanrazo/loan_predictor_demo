c95fc151f6fd9aee5b34d2cac6d96c0657344c9a
Hernan Razo <hernanrazo@gmail.com>
2018-07-07 23:43:11 -0500

    reorganized graphs into one folder

--- loan_prediction.py
+++ loan_prediction.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 #get training data into a dataframe
 df = pd.read_csv('/Users/hernanrazo/pythonProjects/loan_prediction/train_data.csv')
 
+#make string that holds bulk of folder path so
+#I don't have to type the whole thing every time
+graph_folder_path = '/Users/hernanrazo/pythonProjects/loan_prediction/graphs/'
+
 #make a few graphs to visualize the data and
 #get ideas for later manipulation 
 
@@ -13,33 +17,32 @@ df = pd.read_csv('/Users/hernanrazo/pythonProjects/loan_prediction/train_data.cs
 appIncomeHist = plt.figure()
 plt.title('Applicant Income Histogram')
 df['ApplicantIncome'].hist(bins = 50)
-appIncomeHist.savefig('appIncomeHist.png')
+appIncomeHist.savefig(graph_folder_path + 'appIncomeHist.png')
 
 #make a box plot of applicant income
 appIncomeBoxPlot = plt.figure()
 plt.title('Applicant Income Box Plot')
 df.boxplot(column = 'ApplicantIncome')
-appIncomeBoxPlot.savefig('appIncomeBoxPlot.png')
+appIncomeBoxPlot.savefig(graph_folder_path + 'appIncomeBoxPlot.png')
 
 #make another box plot of applicant income but this time,
 #seperate by education levels
 educationBoxPlot = plt.figure()
 plt.title('Applicant Income by Education')
 df.boxplot(column = 'ApplicantIncome', by = 'Education')
-educationBoxPlot.savefig('educationBoxPlot.png')
-
+educationBoxPlot.savefig(graph_folder_path + 'educationBoxPlot.png')
 
 #make a histogram of loan amounts
 loanAmountHist = plt.figure()
 plt.title('Loan Amount Value Histogram')
 df['LoanAmount'].hist(bins = 50)
-loanAmountHist.savefig('loanAmountHist.png')
+loanAmountHist.savefig(graph_folder_path + 'loanAmountHist.png')
 
-#make a histogram of loan amounts
+#make a box plot of loan amounts
 loanAmountBoxPlot = plt.figure()
 plt.title('Loan Amount Value Box Plot')
 df.boxplot(column = 'LoanAmount')
-loanAmountBoxPlot.savefig('loanAmountBoxPlot.png')
+loanAmountBoxPlot.savefig(graph_folder_path + 'loanAmountBoxPlot.png')
 
 #start categorical variable analysis
 #make plot that shows likelihood of approval based on other catagories of 
@@ -50,22 +53,21 @@ ch_ls_graph = plt.figure()
 ch_ls_graph = pd.crosstab(df['Credit_History'], df['Loan_Status'])
 ch_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'], 
 	grid = False, title = 'Likelihood of Approval Based on Credit History')
-plt.savefig('ch_ls_graph.png')
+plt.savefig(graph_folder_path + 'ch_ls_graph.png')
 
 #graph based on education
 e_ls_graph = plt.figure()
 e_ls_graph = pd.crosstab(df['Credit_History'], df['Loan_Status'])
 e_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'],
 	grid = False, title = 'Likelihood of Approval Based on Education')
-plt.savefig('e_ls_graph.png')
-
+plt.savefig(graph_folder_path + 'e_ls_graph.png')
 
 #graph based on marital status
 m_ls_graph = plt.figure()
 m_ls_graph = pd.crosstab(df['Credit_History'], df['Married'])
 m_ls_graph.plot(kind = 'bar', stacked = True, color = ['red', 'green'],
 	grid = False, title = 'Likelihood of Approval Based on Marital Status')
-plt.savefig('m_ls_graph.png')
+plt.savefig(graph_folder_path + 'm_ls_graph.png')
 
 #now deal with empty cells and null values in the dataset
 #check for empty cells and null data 
@@ -94,6 +96,23 @@ df['LoanAmount'].fillna(df[df['LoanAmount'].isnull()].apply(pivot_table,
 	axis = 1), inplace = True)
 
 #now analyze outliers and extreme values
+
+#since the higher valued loans might actually be real and not
+#necessarily outliers, just do a log transformation to cancel
+#them out
+LoanAmount_log = plt.figure()
+plt.title('LoanAmount Log Transformation')
+df['LoanAmount_log'] = np.log(df['LoanAmount'])
+df['LoanAmount_log'].hist(bins = 20)
+LoanAmount_log.savefig(graph_folder_path + 'LoanAmount_log.png')
+
+
+
+
+
+
+
+
 
 
 
